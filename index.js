5a606ef0909da0608a283887370674bb82c1261f
Rodrigo Alarcon <rodalarcon@gmail>
2023-05-28 12:51:36 -0400

    se subió solucion a problema cors

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ const { secretKey } = require("./secretkey")
 
 app.listen(3000, console.log("Servidor encendido"))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+    credentials: true,
+}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 // función middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
