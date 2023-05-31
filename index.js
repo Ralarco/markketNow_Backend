cdd1929accdb00aa8803fe83d77f02ce5a25058b
Rodrigo Alarcon <rodalarcon@gmail>
2023-05-30 20:23:05 -0400

    inst cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { secretKey } = require("./secretkey")
 app.listen(3000, console.log("Servidor encendido"))
 app.use(express.json())
 app.use(cors({
-    origin: "https://marketnow.onrender.com",
+    origin: 'https://marketnow.onrender.com',
     credentials: true,
 }))
 
